0d11f8e0c9ddb93ec7f97457fce9232d5391565a
mervangunduz745-oss <mervangunduz745@gmail.com>
2026-07-21 03:25:41 +0300

    Update Firebase config with appId
    Kardeş bitsin artık bu işkence

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_WEB_API_KEY_HERE",
+appId: "1:78458100943:web:b7b0336931a75988efebba"  
   authDomain: "tamam-d1caa.firebaseapp.com",
   projectId: "tamam-d1caa",
   storageBucket: "tamam-d1caa.firebasestorage.app",
