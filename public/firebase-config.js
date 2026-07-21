9b2069216370780a64d15643ddc24dbe2b710520
mervangunduz745-oss <mervangunduz745@gmail.com>
2026-07-21 03:10:05 +0300

    Firebase yapılandırma dosyasının söz dizimini düzelt

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  appId: "1:78458100943:web:b7b0336931a75988efebba"
+  apiKey: "PASTE_FIREBASE_WEB_API_KEY_HERE",
   authDomain: "tamam-d1caa.firebaseapp.com",
   projectId: "tamam-d1caa",
   storageBucket: "tamam-d1caa.firebasestorage.app",
   messagingSenderId: "78458100943",
-  appId: "1:78458100943:web:a51a57f92bffb6d1efebba"
+  appId: "1:78458100943:web:b7b0336931a75988efebba"
 };
 
 export const cloudConfig = {
